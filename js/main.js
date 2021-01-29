5547259d02ebcf16ab2dd1d0cc4e39345b59dfc5
ahmedskulj00 <66512187+ahmedskulj00@users.noreply.github.com>
2021-01-29 13:54:11 +0100

    Added animation that shows you if the login is successful.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
-const userLogin = document.getElementById("user-box")
-const passLogin = document.getElementById("pass-box")
+var userLogin = document.getElementById("user-box")
+var passLogin = document.getElementById("pass-box")
 var button = document.getElementsByClassName("btn")[0]
+var loginBox = document.getElementById("login-box")
+var welcomeText = document.getElementById("welcome-text")
 
 function checkLogin(){
     var userValue = userLogin.value 
     var passValue = passLogin.value
     if (userValue== "User" || userValue=="user" && passValue == "Pass" || passValue=="pass"){
-        alert("Yaay")
+        hide()
+        var success = document.createElement("div")
+        success.classList.add("success-text")
+        success.innerHTML = "You have successfully logged in!"
+        loginBox.appendChild(success)
     }
-   
+   else{
+       userLogin.style.borderColor = "red"
+       passLogin.style.borderColor = "red"
+   }
 }
 
 button.addEventListener("click", checkLogin)
+
+function hide(){
+   userLogin.style.display = "none"
+   passLogin.style.display = "none"
+   button.style.display = "none"
+   welcomeText.style.display = "none"
+}
+
+
+
